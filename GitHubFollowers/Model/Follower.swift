282d9e9e9cdbedb68998b41db7847356e973527a
Hellen Ferrari <hellenc.ferrari@gmail.com>
2024-02-02 14:03:53 -0800

    Create Follower and User models

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,10 @@
+//
+// Copyright (c) Hellen Ferrari. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,19 @@
+//
+// Copyright (c) Hellen Ferrari. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
