f1861986397c1250ef54968d89d3691b5e92f2ad
Hellen Ferrari <hellenc.ferrari@gmail.com>
2024-03-08 10:28:16 -0800

    User Info View Controller detail

---
+++ GitHubFollowers/Screens/UserInfoViewController.swift
@@ -0,0 +1,25 @@
+//
+// Copyright (c) Hellen Ferrari. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username)
+
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+
+}
